cde51593dda8fb45ac15c5b1d6cfdadecb8bfe3b
Divya090597 <156469276+Divya090597@users.noreply.github.com>
2024-04-02 10:29:35 -0400

    Add files via upload

--- Hierarchial clustering.R
+++ Hierarchial clustering.R
@@ -22,19 +22,15 @@ top_genes <- rna_data[order(gene_vars, decreasing = TRUE), ][1:1000, ]
 dist_matrix <- dist(top_genes[, -1])  # Exclude the 'Gene' column
 hclust_result <- hclust(dist_matrix)
 
-# Convert heatmap_data to a numeric matrix
-heatmap_data_matrix <- as.matrix(heatmap_data)
+# Convert top_genes to a numeric matrix
+top_genes_matrix <- as.matrix(top_genes[,-1])
 
-# Check if heatmap_data_matrix is numeric
-if (!is.numeric(heatmap_data_matrix)) {
-  stop("heatmap_data_matrix is not numeric. Check your data.")
-}
 
 # Define custom color palette
-custom_palette <- colorRampPalette(c("blue", "white", "red")  # Blue to white to red gradient
+custom_palette <- colorRampPalette(c("blue", "white", "red"))  # Blue to white to red gradient
 
 # Generate heatmap
-heatmap(heatmap_data_matrix, col = custom_palette, scale = "row", 
+heatmap(top_genes_matrix, col = heatmap_col, scale = "row", 
         Rowv = as.dendrogram(hclust_result), Colv = NA, 
         margins = c(5,5), main = "Hierarchical Clustering Heatmap",
         xlab = "Samples", ylab = "Genes",
